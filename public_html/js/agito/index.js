9f40b04d92f29ecdd212e3ee650598b13300d12f
fredtma <tshimanga@gmail.com>
2013-07-11 17:00:40 +0200

    2013-07-10

--- public_html/js/agito/index.js
+++ public_html/js/agito/index.js
@@ -3,6 +3,13 @@
  */
 
 tmp = new SET_DISPLAY();
-tmp.navTab({"home":"Home","dealers":["First Dealer","Second Dealer","Third Dealer","Fourth Dealer","hr","View All Dealers"],"salesman":["First Salesman","Second Salesman","Third Salesman","Fourth Salesman","hr","View All Salesman"],"Customers":"Customers","insurance":"Insurance","system":["View Logs","View Clients","Run Import","View Reports","Setup Permission"]});
+tmp.navTab({"home":{"txt":"Home","icon":"icon-home","clss":"active"},"dealers":{"txt":"Dealers","icon":"icon-book","sub":["First Dealer","Second Dealer","Third Dealer","Fourth Dealer","hr","View All Dealers"]},"salesman":{"txt":"Salesman","icon":"icon-briefcase","sub":["First Salesman","Second Salesman","Third Salesman","Fourth Salesman","hr","View All Salesman"]},"customers":{txt:"Customers","icon":"icon-user"},"insurance":{"txt":"Insurance","icon":"icon-folder-open"},"system":{"txt":"System","icon":"icon-cog","sub":["View Logs","View Clients","Run Import","View Reports","Setup Permission"]}});
+tmp.commonSet({"clss":"btn btn-primary"}).btnGroup({
+   "btn-notify":{"title":"My Notification","icon":"icon-inbox icon-white"},
+   "btn-print":{"title":"Print Page","icon":"icon-print icon-white"},
+   "btn-email":{"title":"Email page","icon":"icon-envelope icon-white"},
+   "btn-word":{"title":"Convert to MS Word","icon":"icon-th-large icon-white"},
+   "btn-excel":{"title":"Convert to MS Excel","icon":"icon-th icon-white"}
+});
 
 
--- public_html/js/bibliotheca/res.lambda.js
+++ public_html/js/bibliotheca/res.lambda.js
@@ -15,16 +15,75 @@
  */
 function SET_DISPLAY()
 {
-
-   this.navTab = function(_menu) {
-      if (typeof _menu === 'array'||typeof _menu === 'object')
+   this.Obj='p';
+   /*
+    * the number of menu name will also be the number of container
+    * @type array
+    */
+   var mainMenu = [];
+   var parentAttr, childAttr;
+   /*
+    * used to pass variable that will commonly be used everywhere
+    * @param {object} _obj the object
+    * @returns {object} the object returned
+    */
+   this.commonSet=function(_obj){
+      this.Obj = _obj;
+      return this;
+   }
+   /* This function creates the object based upon the var Attrs
+    * @param {object} _obj the object that will be iterated to extract the string value
+    * @returns {object} return the object that is to be created
+    */
+   this.setObject = function(_obj) {
+      if (typeof(_obj.items)==='array'||typeof(_obj.items)==='object')
       {
-         $.each(_menu, function(key, val){
-            console.log('key='+key+' val='+val+' len='+val.length+' size='+$(val).size()+' typof='+typeof(val));
-            if (typeof(val)==='string')console.log('row1'+val);
-            else $.each(val, function(key2, val2){ console.log('key='+key2+' val='+val2);});
+         $.each(_obj.items, function(key1, item){
+            if(!item.sub){_obj.father(key1,item);}
+            else{_obj.mother(key1,item);};
          });/*endeach*/
       }
+   }
+   /* this function set the navagion object and the values of the Attrs variables
+    * @param {type} _menu
+    * @returns {undefined}
+    */
+   this.navTab = function(_menu) {
+      var ul=create_element({"clss":"nav nav-tabs"},"ul");/*the first element created ul, for the list*/
+      this.setObject({"items":_menu,/*the menu is passed as the first obj*/
+         "father":function(key,item){/*the parent will be the second obj and a function*/
+            mainMenu.push(key);
+            cl=item.clss?item.clss:'';
+            txt=document.createTextNode(item.txt);
+            li=create_element({"clss":cl},"li");
+            a=create_element({"data-toggle":"tab","href":"#tab-"+key},"a");
+            if(item.icon) {i=create_element({"clss":item.icon},"i");a.appendChild(i)}/*add icon if it exist*/
+            a.appendChild(txt);li.appendChild(a);ul.appendChild(li);
+      },"mother":function(key,item){
+            li=create_element({"clss":"dropdown"},"li");
+            a=create_element({"data-toggle":"dropdown","href":"#","clss":"dropdown-toggle"},"a");
+            if(item.icon) {i=create_element({"clss":item.icon},"i");a.appendChild(i)}/*add icon if it exist*/
+            a.appendChild(document.createTextNode(item.txt));
+            b=create_element({"clss":"caret"},"b");
+            a.appendChild(b);li.appendChild(a);
+            ul2=create_element({"clss":"dropdown-menu"},"ul");
+            $.each(item.sub,function(index,val){
+               li2=create_element({},"li");a2=create_element({"data-toggle":"tab","href":"#tab-"+key},"a");txt=document.createTextNode(val);
+               if(val=='hr'){li2.className="divider";txt=document.createTextNode('')}
+               a2.appendChild(txt);li2.appendChild(a2);ul2.appendChild(li2);
+            });
+            ul2.appendChild(li2);li.appendChild(ul2);ul.appendChild(li);
+      }});
+      document.getElementById("nav-main").appendChild(ul);
+   }/*end function*/
+   this.btnGroup=function(_obj){
+      var div=create_element({"clss":"btn-group"},"div");console.log(this);
+      var innerClass=this.Obj.clss;
+      this.setObject({"items":_obj,"father":function(key,item){
+         btn=create_element({"title":item.title,"id":key,"clss":innerClass},"button");
+         i=create_element({"clss":item.icon},"i");btn.appendChild(i);div.appendChild(btn);
+      }});
+      console.log(div);
    }/*end function*/
 }/*end OBJECT*/
 /******************************************************************************/
